1f9055c8a7183334bc838ce6f4bb5d7685abccda
Shreya <shreyathakkar2004@gmail.com>
2026-06-02 13:32:31 +0530

    File Operations in NodeJS

--- 02TUT/index.js
+++ 02TUT/index.js
@@ -1,27 +1,49 @@
-const fs =  require('fs');
+const fsPromises =  require('fs').promises;
 const path = require('path');
 
-fs.readFile(path.join(__dirname, 'Files', 'starter.txt'), 'utf-8', (err, data) => {
-    if(err) throw err;
-    console.log(data);
-})
 
-console.log('This will run before the file is read');
+async function fileOps() {
+    try {
+        const data = await fsPromises.readFile(path.join(__dirname, 'Files', 'starter.txt'), 'utf-8');
+        console.log(data);
 
-fs.writeFile(path.join(__dirname, 'Files', 'reply.txt'), 'Nice to meet you!', (err) => {
-    if(err) throw err;
-    console.log("Write Operation complete");
+        await fsPromises.unlink(path.join(__dirname, 'Files', 'starter.txt'));
+        await fsPromises.writeFile(path.join(__dirname, 'Files', 'promiseWrite.txt'), 'Hello my name is Shreya' + '\nHello From the Promis WriteFile');
+        await fsPromises.appendFile(path.join(__dirname, 'Files', 'promiseWrite.txt'), '\nHello From the Promis AppendFile');
+        await fsPromises.rename(path.join(__dirname, 'Files', 'promiseWrite.txt'), path.join(__dirname, 'Files', 'promiseComplete.txt'));
+         const Newdata = await fsPromises.readFile(path.join(__dirname, 'Files', 'promiseComplete.txt'), 'utf-8');
+        console.log(Newdata);
+    }catch (err) {
+        console.error(err);
+    }
+}
+fileOps();
 
-    fs.appendFile(path.join(__dirname, 'Files', 'reply.txt'), '\nTesting the new Line is working', (err) => {
-    if(err) throw err;
-    console.log("Append Operation complete");
 
-    fs.rename(path.join(__dirname, 'Files', 'reply.txt'), path.join(__dirname, 'Files', 'newReply.txt'), (err) => {
-    if(err) throw err;
-    console.log("Rename Operation complete");
-})
-})
-});
+
+
+
+// fs.readFile(path.join(__dirname, 'Files', 'starter.txt'), 'utf-8', (err, data) => {
+//     if(err) throw err;
+//     console.log(data);
+// })
+
+// console.log('This will run before the file is read');
+
+// fs.writeFile(path.join(__dirname, 'Files', 'reply.txt'), 'Nice to meet you!', (err) => {
+//     if(err) throw err;
+//     console.log("Write Operation complete");
+
+//     fs.appendFile(path.join(__dirname, 'Files', 'reply.txt'), '\nTesting the new Line is working', (err) => {
+//     if(err) throw err;
+//     console.log("Append Operation complete");
+
+//     fs.rename(path.join(__dirname, 'Files', 'reply.txt'), path.join(__dirname, 'Files', 'newReply.txt'), (err) => {
+//     if(err) throw err;
+//     console.log("Rename Operation complete");
+// })
+// })
+// });
 
 // fs.appendFile(path.join(__dirname, 'Files', 'test.txt'), 'Testing Text', (err) => {
 //     if(err) throw err;
---
+++ 02TUT/stream.js
@@ -0,0 +1,3 @@
+//Now if we have large files sometimes it is not good to grab all of the data at once
+//It could be too much just like moving all the water in the swimming pool at once, we can use streams to move the data in smaller chunks
+//Streams are also used for real time data like video streaming, audio streaming, etc
